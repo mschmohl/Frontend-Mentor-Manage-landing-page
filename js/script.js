fb9b9b4e685919b9b23a4220170368ec98d9e807
Melissa Schmohl <101364208+mschmohl@users.noreply.github.com>
2022-07-12 11:31:42 -0500

    Added functioning slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('menu-btn')
+// Hamburger
 const nav = document.getElementById('menu')
 
 btn.addEventListener('click', () => {
@@ -6,3 +7,54 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 })
+
+// Slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+    speed:400,
+    spaceBetween:100,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+// Email Validation
+function validation() {
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern)) {
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "Your Email Address is Valid.";
+        text.style.color = "#00ff00";
+    } else {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        text.innerHTML = "Please Enter a Valid Email Address";
+        text.style.color = "#ff0000";
+    }
+    if (email == "") {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        text.innerHTML = "";
+        text.style.color = "#00ff00";
+    }
+}
